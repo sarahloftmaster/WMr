6ee3ee0b239408b6890a5e10574071f0e34f346b
whryan <williamhryan@gmail.com>
2018-02-19 22:44:17 -0800

    modify test phase

--- static/js/custom_plugins/create_test_phase.js
+++ static/js/custom_plugins/create_test_phase.js
@@ -8,12 +8,24 @@
 
 console.log('Begin test phase creation!');
 
+ var test_instructs_oneline = '<p>Great! You are almost done with this experiment.</p><p>It is time to test what you have learned.</p><p>&nbsp</p><p>You will see one image on the screen at a time. Enter the key that gave you the greatest number of rewards for each image previously. If you are not sure which key to press, just go with your gut instinct!</p><p>During this set of trials you will NOT receive feedback about your responses. However, your performance will still count towards your bonus, so be as accurate as possible.</p><p>As before, respond to each image by pressing one of the three buttons on the keyboard:<p> j, k or l</p> with your dominant hand.</p><p>&nbsp</p><p>[Press SPACEBAR to continue]</p>]'
+
 	/* This is the instructions block for the test phase*/
 	var test_phase_instructions = {
-		type: "instructions",
-		pages: test_instructs,
-		key_forward: test_instructs_fwd,
-		time_limit: 3000
+		type: "categorize", 
+		stimulus: test_instructs_oneline,
+		is_html: true,
+		choices: [32, 74, 75, 76],
+		key_answer: 32,
+		timing_response: 180000,
+		show_stim_with_feedback: false,
+		show_feedback_on_timeout: false,
+		timeout_message: '<p> Test phase about to begin! </p>',
+		timing_feedback_duration: 1500,
+		prompt: '',
+		incorrect_text: '<p> Test phase about to begin! </p>',
+		correct_text: '<p> Test phase about to begin! </p>',
+		timing_stim: -1
 	};
 
 	/* this is the test phase for the experiment */
